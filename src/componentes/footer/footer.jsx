9bf7f91af0976fd7cc1e96488ac4e71376083d31
patriky1 <patrikybrito@gmail.com>
2022-05-26 20:24:11 -0300

    update

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -14,13 +14,18 @@ function Footer() {
                 <Col className="colorfooter" lg={6}>
                         
                     <h1 className="m-5">e-company.</h1>
-                    <p className=" m-5"> © Todos os direitos reservados, 2022.</p>
+                    
                 </Col>
                 <Col lg={6} className="mt-5"><a className="item1" href="#portfolio">Company</a>
                 <Col className="mt-2">  <a className="item" href="home">Home</a></Col>
                 <Col className="mt-2">  <a className="item" href="sobre">Sobre</a></Col>
                 <Col className="mt-2 mb-5">  <a className="item" href="mailto:patrikybrito@gmail.com">Contato</a></Col>
                 </Col>
+                
+                
+            </Row>
+            <Row>
+            <p className="direitos" className="direitos m-5"> © Todos os direitos reservados, 2022.</p>
             </Row>
         </Container>
       </>

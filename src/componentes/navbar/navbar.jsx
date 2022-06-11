03c311d1259d4743db80dfe185801cb8b5805b94
patriky1 <patrikybrito@gmail.com>
2022-06-10 21:05:12 -0300

    up

--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -10,7 +10,7 @@ function Menu() {
         <Row className="colapse" lg={6}>
           <Col className="itens" lg={5}>
             <a className="item" href="#portfolio">
-              port
+              portifólio
             </a>
             <a className="item" href="#education">
               Education

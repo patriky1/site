f0abe5270dcb140796c7ccf0515de5cd111cbd05
patriky1 <patrikybrito@gmail.com>
2022-07-07 19:48:55 -0300

    up

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
     <>
       <Container fluid className="mt-5">
         <Row>
-          <Col lg={8} className="mobileresize">
+          <Col lg={8} className="mobileresize mt-5">
             
               <img className="imagemlogo" src={Logo} alt="..."/>
             
